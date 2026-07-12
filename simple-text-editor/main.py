0d4bffbe4c992962e935588cb740efc3ff50ebec
tolanny-studio <hellotolannystudio@gmail.com>
2026-07-12 07:17:39 +0100

    write inputs to file

--- simple-text-editor/main.py
+++ simple-text-editor/main.py
@@ -12,3 +12,12 @@ def get_input():
             break
         words.append(word + "\n")
     return words
+
+
+def write_file():
+    file_name = input("Enter file name to open or create: ").strip()
+    try:
+        with open(file_name, "w") as file:
+            file.writelines(get_input())
+    except OSError:
+        print(f"{file_name} not present")
