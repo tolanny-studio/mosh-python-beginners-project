8b2c6c30cebf7f224a24b7e0d17058ea19ae98e9
tolanny-studio <hellotolannystudio@gmail.com>
2026-07-15 12:40:38 +0100

    Validate include choices

--- password-generator/main.py
+++ password-generator/main.py
@@ -34,24 +34,33 @@ def password_generator(
 def validate_password_length(prompt):
     while True:
         try:
-            password_length = int(input(prompt))
+            password_length = int(input(f"\n{prompt}"))
             if password_length < 5:
-                print("Invalid Input⛔. Password should be more than four digit")
+                print("\nInvalid Input ⛔ Password should be more than four digit")
                 continue
             return password_length
         except ValueError:
-            print("Invalid input⛔. Enter an integer")
+            print("\nInvalid input ⛔ Enter an integer")
+
+
+def validate_include_choice(prompt):
+    while True:
+        include_option = input(f"\n{prompt}").lower()
+        if include_option not in ("y", "n"):
+            print("\nInvalid answer ⛔ Enter y/n ")
+            continue
+        return include_option == "y"
 
 
 def main():
     password_length = validate_password_length("Enter password length: ")
-    include_upper_case = (
-        input("Do you want to include an uppercase (y/n): ").lower() == "y"
+    include_upper_case = validate_include_choice(
+        "Do you want to include an uppercase (y/n): "
     )
-    include_special_character = (
-        input("Do you want to include a special character (y/n): ").lower() == "y"
+    include_special_character = validate_include_choice(
+        "Do you want to include a special character (y/n): "
     )
-    include_number = input("Do you want to include a number (y/n): ").lower() == "y"
+    include_number = validate_include_choice("Do you want to include a number (y/n): ")
     password = password_generator(
         password_length, include_upper_case, include_special_character, include_number
     )
