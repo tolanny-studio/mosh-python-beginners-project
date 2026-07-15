c6bf83ce66b65b1225a9b1a1380096f1e5efb892
tolanny-studio <hellotolannystudio@gmail.com>
2026-07-15 22:22:34 +0100

    split code into functionalities

--- word-guessing-game/main.py
+++ word-guessing-game/main.py
@@ -1,31 +1,61 @@
 import random
 
-word_list = []
-with open("word.txt", "r") as file:
-    for line in file:
-        word_list.append(line.strip())
-print(word_list)
 
-random_word = random.choice(word_list)
-print(random_word)
-
-blanks = ""
-
-for _ in random_word:
-    blanks += "_"
-
-print(blanks)
-
-guess = "a"
-
-list_blanks = list(blanks)
-
-print(list_blanks)
-
-for index, char in enumerate(random_word):
-    if char == guess:
-        list_blanks[index] = guess
+def get_word_list():
+    word_list = []
+    with open("word.txt", "r") as file:
+        for line in file:
+            word_list.append(line.strip())
+    return word_list
 
 
-print(random_word)
-print("".join(list_blanks))
+def select_random_word():
+    return random.choice(get_word_list())
+
+
+def get_guess_letter():
+    return input("Enter a letter🔤: ").lower()
+
+
+def retry_game():
+    while True:
+        retry = input("Do you want to retry 🔄️: ").lower()
+        if retry not in ("y", "n"):
+            print("Invalid input")
+            continue
+        return retry == "y"
+
+
+def main():
+    lives = 6
+    random_word = select_random_word()
+    blanks = ""
+    for _ in random_word:
+        blanks += "_"
+    # print(blanks)
+    list_blanks = list(blanks)
+    while True:
+        guess = get_guess_letter()
+        for index, char in enumerate(random_word):
+            if char == guess:
+                print("Good guess. Keep it up")
+                list_blanks[index] = guess
+
+        print(random_word)
+        blanks = "".join(list_blanks)
+        print(blanks)
+        lives -= 1
+
+        if lives == 0:
+            print("Game Over ⛔")
+            break
+        if "_" not in blanks:
+            print("Kudos 👊 You made it.")
+            break
+    if retry_game():
+      main()
+      lives = 6
+
+
+if __name__ == "__main__":
+    main()
