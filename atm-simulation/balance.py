2bfe5c0ba7c29d77cdcf9a52c02d17546989b5b3
tolanny-studio <hellotolannystudio@gmail.com>
2026-07-20 17:55:54 +0100

    Refactor: repositon try function

--- atm-simulation/balance.py
+++ atm-simulation/balance.py
@@ -6,20 +6,20 @@ logger = logging.getLogger(__name__)
 
 
 def read_balance():
-    with open(BALANCE_FILE, "r") as file:
-        try:
+    try:
+        with open(BALANCE_FILE, "r") as file:
             return int(file.read())
-        except FileNotFoundError:
-            logger.warning("Balance file not found ⛔ Initializing balance to zero")
-            return 0
-        except ValueError as e:
-            logger.error(f"Invalid balance value ,{e}")
-            return 0
+    except FileNotFoundError:
+        logger.warning("Balance file not found ⛔ Initializing balance to zero")
+        return 0
+    except ValueError as e:
+        logger.error(f"Invalid balance value ,{e}")
+        return 0
 
 
 def write_balance(current_balance):
     try:
         with open(BALANCE_FILE, "w") as file:
-                file.write(str(current_balance))
+            file.write(str(current_balance))
     except FileNotFoundError:
         logger.error("Cannot write balance: file not found")
