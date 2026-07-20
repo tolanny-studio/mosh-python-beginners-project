ff1dc689b5efd877f5d73cd5581c654378b2997d
tolanny-studio <hellotolannystudio@gmail.com>
2026-07-20 17:53:13 +0100

    Refactor: use logging module to highlight exceptions

--- atm-simulation/balance.py
+++ atm-simulation/balance.py
@@ -1,26 +1,25 @@
 from pathlib import Path
+import logging
 
 BALANCE_FILE = Path(__file__).parent / "balance.txt"
+logger = logging.getLogger(__name__)
 
 
 def read_balance():
     with open(BALANCE_FILE, "r") as file:
-            try:
-                return int(file.read())   
-            except FileNotFoundError:
-                print("File not found")
-                return 0
-            except ValueError as e:
-                print("Invalid balance format in file")
-                return 0
-
-
+        try:
+            return int(file.read())
+        except FileNotFoundError:
+            logger.warning("Balance file not found ⛔ Initializing balance to zero")
+            return 0
+        except ValueError as e:
+            logger.error(f"Invalid balance value ,{e}")
+            return 0
 
 
 def write_balance(current_balance):
     try:
         with open(BALANCE_FILE, "w") as file:
-            if current_balance:
                 file.write(str(current_balance))
     except FileNotFoundError:
-        print("file not found")
+        logger.error("Cannot write balance: file not found")
